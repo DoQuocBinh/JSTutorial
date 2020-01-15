ab1309986c27164b5507e886e96b74c4c46ea176
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-01-15 17:27:59 +0700

    add port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var http = require('http');
 
+var port = process.env.PORT || 8080;
+
+
 http.createServer((request,response)=>{
     response.writeHead(200,{'content-type':'text/html'});
     response.end('Hello world1233');
-}).listen(8080);
+}).listen(port);
